564a2212162b1b52fc018df5833bb785d6558d28
MUmairqureshi <umairqadeer586@gmail.com>
2022-04-18 06:46:47 -1200

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,3 @@
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
-// import 'firebase/compat/firestore';
-
-// const app = firebase.initializeApp({
-//   apiKey: "AIzaSyD2FGV1f456gIktsE-Q27w0V3rbhlgITkg",
-//   authDomain: "test-dev-eec50.firebaseapp.com",
-//   projectId: "test-dev-eec50",
-//   storageBucket: "test-dev-eec50.appspot.com",
-//   messagingSenderId: "825626825081",
-//   appId: "1:825626825081:web:6a0ee787fbbea3bc3f9fa8"
-// })
-
-// export const auth = app.auth()
-// export default app
-
-
-
-
-
-
-
-
-
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
